9080b7ea921577a8c2bc4a163de8e24d95f8ec8a
jaebungs <jaebungs@gmail.com>
2023-12-03 16:01:09 -0500

    use variable instead of hard code URL

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { hostedUIURL } from '../urls/hostedUI'
 
 const Navigation = () => {
   return (
     <div>
-      <a href='https://crochetapp.auth.us-east-2.amazoncognito.com/login?client_id=5qlmp0hq79slqcjtmqf3hi6hkj&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Flocalhost%3A3001'>
+      <a href={hostedUIURL}>
         Sign In / Sign Up
       </a>
     </div>
---
+++ urls/hostedUI.js
@@ -0,0 +1,2 @@
+export const hostedUIURL  = 'https://crochetapp.auth.us-east-2.amazoncognito.com/login?client_id=5qlmp0hq79slqcjtmqf3hi6hkj&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Flocalhost%3A3001'
+
